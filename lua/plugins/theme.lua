97df1f587d0ef2413fcba47f60d78a5aa8d8f8a3
kurian-thomas <31894790+kurian-thomas@users.noreply.github.com>
2026-06-09 01:46:08 -0700

    Update theme.lua to comment out theme settings
    Commented out theme configuration for LazyVim and gruvbox.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,11 @@
 /home/nova/.config/omarchy/current/theme/neovim.lua
+
+-- return {
+-- 	{ "ellisonleao/gruvbox.nvim" },
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "gruvbox",
+-- 		},
+-- 	},
+-- }
